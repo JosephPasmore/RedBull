3d1ba4b8f1b5df2b4c22b452b6f82937182bd549
GabeParra <gabrielparra333@gmail.com>
2018-07-22 02:22:07 -0500

    fixed merge conflicts and updated review classes

--- src/main/java/com/lit/dao/AddReview.java
+++ src/main/java/com/lit/dao/AddReview.java
@@ -1,83 +1,45 @@
 package com.lit.dao;
 
+import com.lit.entity.Review;
 import com.lit.entity.WritableReview;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class AddReview {
-     // JDBC driver name and database URL
-     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-     static final String DB_URL = "jdbc:mysql://Josephs-MacBook-Air.local:3306/";
 
-     // Database credentials
-     static final String USER = "root";
-     static final String PASS = "1234";
+    public static int executeAddReview(WritableReview review) throws Exception {
 
-    WritableReview review;
+        Connection dbConnection;
+        PreparedStatement preparedStatement;
 
-     public AddReview(WritableReview reviewToAdd)
-     {
-        review = reviewToAdd;
-     }
+        String sql = "INSERT INTO review (pictureId, commentId, locationId, rating, availability, description, uploadDateTime)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    public int execute()
-     {
-        Connection conn = null;
-        Statement stmt = null;
+        dbConnection = getDBConnection();
+        preparedStatement = dbConnection.prepareStatement(sql);
+        preparedStatement.setInt(1, review.getPictureId());
+        preparedStatement.setInt(2, review.getCommentIdId());
+        preparedStatement.setInt(3, review.getLocationId());
+        preparedStatement.setInt( 4, review.getRating());
+        preparedStatement.setString( 5, review.getAvailability());
+        preparedStatement.setString( 6, review.getDescription());
+        preparedStatement.setString( 7, review.getUploadDateTime());
+        return preparedStatement.executeUpdate();
+    }
 
-         try {
-             Class.forName("com.mysql.jdbc.Driver");
-         } catch (ClassNotFoundException e) {
-             e.printStackTrace();
-         }
-         System.out.print("\nConnecting to database...");
-         try {
-             conn = DriverManager.getConnection(DB_URL, USER, PASS);
-         } catch (SQLException e) {
-             e.printStackTrace();
-         }
-         System.out.println(" SUCCESS!\n");
+    private static Connection getDBConnection() throws Exception
+    {
 
+        Connection dbConnection = null;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-            try {
-                stmt = conn.createStatement();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            String sql = "INSERT INTO review (pictureId, commentId, locationId, rating, availability, description, uploadDateTime)" +
-                    "VALUES (?, ?, ?, ?, ?, ?, ?)";
-//         pictureId
-//         commentId
-//         locationId
-//         userId
-//         rating
-//         availability
-//         description
-//         uploadDateTime
-
-         try {
-             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-             preparedStatement.setInt(1, review.getPictureId());
-             preparedStatement.setInt(2, review.getCommentIdId());
-             preparedStatement.setInt(3, review.getLocationId());
-             preparedStatement.setInt( 4, review.getRating());
-             preparedStatement.setString( 5, review.getAvailability());
-             preparedStatement.setString( 6, review.getDescription());
-             preparedStatement.setString( 7, review.getUploadDateTime());
-             preparedStatement.executeUpdate();
-         } catch (SQLException e) {
-             e.printStackTrace();
-         }
-
-            try {
-              return stmt.executeUpdate(sql);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-         return -1;
-     }
+        dbConnection = DriverManager.getConnection("jdbc:mysql://75.98.125.90/napCore?serverTimezone=UTC&"
+                + "user=root&password=password");
+        return dbConnection;
+    }
 }
+
+
+
--- src/main/java/com/lit/rest/AddNapReview.java
+++ src/main/java/com/lit/rest/AddNapReview.java
@@ -6,7 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.lit.Track;
+import com.lit.dao.AddReview;
+import com.lit.entity.WritableReview;
 
 @Path("/service")
 public class AddNapReview {
@@ -14,8 +15,13 @@ public class AddNapReview {
     @POST
     @Path("/addNapReview")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addNapReview(Track track) {
-        String result = "Track saved : " + track;
-        return Response.status(201).entity(result).build();
+    public Response addNapReview(WritableReview review) {
+        int rows = 0;
+        try {
+            rows = AddReview.executeAddReview(review);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.status(201).entity(rows).build();
     }
 }
