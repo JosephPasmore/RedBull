196194ef043aa8aa191ec420e03e94b8ef64710e
Joseph <Broseph@Josephs-MacBook-Air.local>
2018-07-22 00:16:34 -0500

    Refactor Writable Review

--- src/main/java/com/lit/entity/WritableReview.java
+++ src/main/java/com/lit/entity/WritableReview.java
@@ -1,28 +1,18 @@
-package com.lit.entity;
+package com.lit;
 
 public class WritableReview
 {
-    private final int reviewId;
-    private final int pictureId;
-    private final int commentId;
-    private final int locationId;
-    private final int userId;
-    private final int rating;
-    private final String availability;
-    private final String description;
-    private final String uploadDateTime;
+    private int reviewId;
+    private int pictureId;
+    private int commentId;
+    private int locationId;
+    private int userId;
+    private int rating;
+    private String availability;
+    private String description;
+    private String uploadDateTime;
 
-    private WritableReview(WritableReviewBuilder builder)
-    {
-        this.reviewId = builder.reviewId;
-        this.pictureId = builder.pictureId;
-        this.commentId = builder.commentId;
-        this.locationId = builder.locationId;
-        this.userId = builder.userId;
-        this.rating = builder.rating;
-        this.availability = builder.availability;
-        this.description = builder.description;
-        this.uploadDateTime = builder.uploadDateTime;
+    public WritableReview() {
     }
 
     public int getReviewId()
@@ -30,121 +20,72 @@ public class WritableReview
         return reviewId;
     }
 
+    public int getUserId()
+    {
+        return userId;
+    }
+
     public int getPictureId()
     {
         return pictureId;
     }
 
-    public int getCommentIdId()
-    {
+    public int getCommentIdId() {
         return commentId;
     }
 
-    public int getLocationId()
-    {
+    public int getLocationId() {
         return locationId;
     }
 
-    public int getUserIdId()
-    {
-        return userId;
-    }
-
-    public int getRating()
-    {
+    public int getRating() {
         return rating;
     }
 
-    public String getAvailability()
-    {
+    public String getAvailability() {
         return availability;
     }
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return description;
     }
 
-    public String getUploadDateTime()
-    {
+    public String getUploadDateTime() {
         return uploadDateTime;
     }
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
 
-    public static class WritableReviewBuilder
-    {
-        private int reviewId;
-        private int pictureId;
-        private int commentId;
-        private int locationId;
-        private int userId;
-        private int rating;
-        private String availability;
-        private String description;
-        private String uploadDateTime;
+    public void setPictureId(int pictureId) {
+        this.pictureId = pictureId;
+    }
 
-        public WritableReviewBuilder()
-        {
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
 
-        }
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
 
-        // Multiple Constructors for each member variable
-        public WritableReviewBuilder withReviewId(int reviewId)
-        {
-            this.reviewId = reviewId;
-            return this;
-        }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-        public WritableReviewBuilder withPictureId(int pictureId)
-        {
-            this.pictureId = pictureId;
-            return this;
-        }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 
-        public WritableReviewBuilder withCommentId(int commentId){
-            this.commentId = commentId;
-            return this;
-        }
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
 
-        public WritableReviewBuilder withLocationId(int locationId)
-        {
-            this.locationId = locationId;
-            return this;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        public WritableReviewBuilder withUserId(int userId)
-        {
-            this.userId = userId;
-            return this;
-        }
-
-        public WritableReviewBuilder withRating(int rating)
-        {
-            this.rating = rating;
-            return this;
-        }
-
-        public WritableReviewBuilder withAvailability(String availability)
-        {
-            this.availability = availability;
-            return this;
-        }
-
-        public WritableReviewBuilder withDescription(String description)
-        {
-            this.description = description;
-            return this;
-        }
-
-        public WritableReviewBuilder withUploadDateTime(String uploadDateTime)
-        {
-            this.uploadDateTime = uploadDateTime;
-            return this;
-        }
-
-        // The only method to initiate Review class
-        public WritableReview build()
-        {
-            return new WritableReview(this);
-        }
+    public void setUploadDateTime(String uploadDateTime) {
+        this.uploadDateTime = uploadDateTime;
     }
 }
